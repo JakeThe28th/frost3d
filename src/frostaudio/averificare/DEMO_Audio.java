b5fd09abe3b836686f8bad4e977c62e85cb025af
JakeThe28th <42324960+JakeThe28th@users.noreply.github.com>
2025-11-08 01:34:32 +0000

    move audio here

---
+++ src/frostaudio/averificare/DEMO_Audio.java
@@ -0,0 +1,18 @@
+package frostaudio.averificare;
+
+public class DEMO_Audio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Test loading an audio file and playing it in the default audio device
+		
+		// Test volume, seek, etc
+		
+		// Test switching audio devices
+		
+		// TODO
+
+	}
+
+}
