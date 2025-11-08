d772b9782901a891f33dec33585a737852eccb73
JakeThe28th <42324960+JakeThe28th@users.noreply.github.com>
2025-11-08 01:34:40 +0000

    new rectangle methods

--- src/frost3d/utility/Rectangle.java
+++ src/frost3d/utility/Rectangle.java
@@ -3,7 +3,7 @@ package frost3d.utility;
 import org.joml.Vector2i;
 
 public record Rectangle(int left, int top, int right, int bottom) {
-	
+
 	public boolean contains(int x, int y) {
 		return (x > left && x < right) && (y > top && y < bottom);
 	}
@@ -43,6 +43,29 @@ public record Rectangle(int left, int top, int right, int bottom) {
 				(int) (top + (height() * tp)), 
 				(int) (left + (width() * rp)), 
 				(int) (top + (height() * bp)));
+	}
+
+	public Rectangle constrain_to(Rectangle other) {
+		int left 	= this.left;
+		int top 	= this.top;
+		int right 	= this.right;
+		int bottom 	= this.bottom;
+		if (left 	< other.left) 	left = other.left;
+		if (top 	< other.top) 	top = other.top;
+		if (right 	> other.right) 	right = other.right;
+		if (bottom 	> other.bottom) bottom = other.bottom;
+		return new Rectangle(left, top, right, bottom);
+	}
+
+	public boolean intersects(Rectangle other) {
+		return 	(right 	> other.left) 	&&
+				(left 	< other.right) 	&&
+				(bottom > other.top) 	&&
+				(top 	< other.bottom);
+	}
+
+	public Rectangle offset(int offset_x, int offset_y) {
+		return new Rectangle(left + offset_x, top + offset_y, right + offset_x, bottom + offset_y);
 	} 
 	
 }
