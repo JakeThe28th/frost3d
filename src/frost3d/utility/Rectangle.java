3422f3a78b00ab274696a3b59eaf8001006b8e37
JakeThe28th <42324960+JakeThe28th@users.noreply.github.com>
2026-07-18 20:26:27 +0100

    Rectangle.contains() now returns true in certain 'edge' cases xD
    In previous versions, if you were to draw a rectangle and then check if the mouse is in it, it would return true correctly in all cases, except if the mouse was in the bottom edge or left edge row/column of pixels. it will now return true in these cases

--- src/frost3d/utility/Rectangle.java
+++ src/frost3d/utility/Rectangle.java
@@ -6,7 +6,7 @@ import org.joml.Vector2i;
 public record Rectangle(int left, int top, int right, int bottom) {
 
 	public boolean contains(int x, int y) {
-		return (x > left && x < right) && (y > top && y < bottom);
+		return (x >= left && x < right) && (y > top && y <= bottom);
 	}
 	
 	public boolean contains(Vector2i p) {
